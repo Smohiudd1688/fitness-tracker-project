a013ed2699ffc72d4ed848bec902650aa678b8d9
Sana Mohiuddin <smohiuddin1688@gmail.com>
2023-01-08 17:39:58 -0700

    add errors to signup form

--- client/src/Forms/SignupForm.js
+++ client/src/Forms/SignupForm.js
@@ -7,7 +7,7 @@ function SignupForm({setCurrentUser}) {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [monthlyGoal, setMonthlyGoal] = useState();
-    const [errors, setErrors] = useState("");
+    const [errors, setErrors] = useState([]);
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -27,9 +27,9 @@ function SignupForm({setCurrentUser}) {
         })
         .then(res => {
             if(res.ok) {
-                res.json().then(setCurrentUser)
+                res.json().then(user => setCurrentUser(user))
             } else {
-                res.json().then(e => console.log(e.errors[0]))
+                res.json().then(e => setErrors(e.errors))
             }
         })
     }
@@ -58,23 +58,32 @@ function SignupForm({setCurrentUser}) {
         setMonthlyGoal(event.target.value);
     }
 
+    const renderErrors = errors.map((error, index) => {
+        return (
+            <p className="errors" key={index}>* {error}</p>
+        );
+    })
+
     return (
-        <form className="logForms" onSubmit={handleSubmit}>
-            <h3>Sign Up for The Fit Life</h3>
-            <label htmlFor="fname">First Name: </label>
-            <input onChange={handleFirstNameChange} type="text" id="fname" name="fname" value={firstName} /><br></br><br></br>
-            <label htmlFor="lname">Last Name: </label>
-            <input onChange={handleLastNameChange} type="text" id="fname" name="fname" value={lastName} /><br></br><br></br>
-            <label htmlFor="username">Username: </label>
-            <input onChange={handleUsernameChange} type="text" id="username" name="username" value={username} /><br></br><br></br>
-            <label htmlFor="pass">Password: </label>
-            <input onChange={handlePassChange} type="password" id="pass" name="pass" value={password} /><br></br><br></br>
-            <label htmlFor="confirmPass">Confirm Password: </label>
-            <input onChange={handleConfirmPassChange} type="password" id="confirmPass" name="confirmPass" value={confirmPassword} /><br></br><br></br>
-            <label htmlFor="monthlyGoal">Monthly Workout Goal: </label>
-            <input onChange={handleMonthlyGoalChange} type="text" id="monthlyGoal" name="monthlyGoal" value={monthlyGoal} /><br></br><br></br>
-            <input type="submit" />
-        </form>
+        <div>
+            {errors.length !== 0 ? renderErrors : null}
+            <form className="logForms" onSubmit={handleSubmit}>
+                <h3>Sign Up for The Fit Life</h3>
+                <label htmlFor="fname">First Name: </label>
+                <input onChange={handleFirstNameChange} type="text" id="fname" name="fname" value={firstName} /><br></br><br></br>
+                <label htmlFor="lname">Last Name: </label>
+                <input onChange={handleLastNameChange} type="text" id="lname" name="lname" value={lastName} /><br></br><br></br>
+                <label htmlFor="username">Username: </label>
+                <input onChange={handleUsernameChange} type="text" id="username" name="username" value={username} /><br></br><br></br>
+                <label htmlFor="pass">Password: </label>
+                <input onChange={handlePassChange} type="password" id="pass" name="pass" value={password} /><br></br><br></br>
+                <label htmlFor="confirmPass">Confirm Password: </label>
+                <input onChange={handleConfirmPassChange} type="password" id="confirmPass" name="confirmPass" value={confirmPassword} /><br></br><br></br>
+                <label htmlFor="monthlyGoal">Monthly Workout Goal: </label>
+                <input onChange={handleMonthlyGoalChange} type="text" id="monthlyGoal" name="monthlyGoal" value={monthlyGoal} /><br></br><br></br>
+                <input type="submit" />
+            </form>
+        </div>
     );
 }
 
